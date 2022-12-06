1ca0bcc29e1350427c686685f3cf35d714917b30
A. Mioranza <amioranza@mdcnet.ninja>
2022-12-06 13:28:26 -0300

    alb logs

---
+++ iac/infra-live/alb_logs.tf
@@ -0,0 +1,42 @@
+resource "aws_s3_bucket" "default" {
+  bucket = "tt-alb-${local.env}-logs"
+  acl    = "private"
+
+  versioning {
+    enabled = true
+  }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+
+  force_destroy = true
+}
+
+resource "aws_s3_bucket_policy" "default" {
+  bucket = aws_s3_bucket.default.id
+  policy = data.aws_iam_policy_document.default.json
+}
+
+data "aws_iam_policy_document" "default" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "AWS"
+      identifiers = [data.aws_elb_service_account.default.arn]
+    }
+
+    actions = [
+      "s3:PutObject",
+    ]
+
+    resources = [
+      "arn:aws:s3:::tt-alb-${local.env}-logs/*",
+    ]
+  }
+}
