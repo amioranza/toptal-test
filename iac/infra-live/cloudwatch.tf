d68d1412bd042a88836dc9541aead218610ba7ca
A. Mioranza <amioranza@mdcnet.ninja>
2022-12-06 09:55:30 -0300

    fix logs

--- iac/infra-live/cloudwatch.tf
+++ iac/infra-live/cloudwatch.tf
@@ -1,5 +1,5 @@
 resource "aws_cloudwatch_log_group" "logs" {
   for_each          = toset(local.app_modules)
-  name              = "${each.value}-${local.env}"
+  name              = "/application/${each.value}/${local.env}"
   retention_in_days = 120
 }
