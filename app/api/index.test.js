ee3127233771675da77e003b6473938082bf0a7b
A. Mioranza <amioranza@mdcnet.ninja>
2022-12-06 23:18:29 -0300

    Add app tests

--- app/api/index.test.js
+++ app/api/index.test.js
@@ -5,3 +5,19 @@ test("DBUSER env var not set", () => {
 test("DB env var not set", () => {
   expect(process.env.DB).toEqual(undefined);
 });
+
+test("PORT env var not set", () => {
+  expect(process.env.PORT).toEqual(undefined);
+});
+
+test("DBPASS env var not set", () => {
+  expect(process.env.DBPASS).toEqual(undefined);
+});
+
+test("DBHOST env var not set", () => {
+  expect(process.env.DBHOST).toEqual(undefined);
+});
+
+test("DBPORT env var not set", () => {
+  expect(process.env.DBPORT).toEqual(undefined);
+});
